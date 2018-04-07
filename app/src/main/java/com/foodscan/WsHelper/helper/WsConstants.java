c34809dd77338dbc0d0d4ce2a26cc7056e2e3586
Pierre-Henry Soria <hi@ph7.me>
2018-04-07 18:14:05 +0100

    Update Service API URL

--- app/src/main/java/com/foodscan/WsHelper/helper/WsConstants.java
+++ app/src/main/java/com/foodscan/WsHelper/helper/WsConstants.java
@@ -3,12 +3,8 @@ package com.foodscan.WsHelper.helper;
 
 public class WsConstants {
 
-    //public static String BASE_URL = "https://lifyzer.com/api/";
-    public static String BASE_URL = "http://europa.narola.online/";
-
-    //public static String SERVICE_URL = BASE_URL + "pg/FoodScanApp/FoodScanAppService.php?Service=";
-    public static String SERVICE_URL = BASE_URL + "pg/FoodScanApp/WS/FoodScanAppService.php?Service=";
-
+    public static String BASE_URL = "http://lifyzer.com/api/";
+    public static String SERVICE_URL = BASE_URL + "WS/FoodScanAppService.php?Service=";
 
     public static String REFRESH_TOKEN = "refreshToken";
     public static String LOGIN = "Login";
@@ -21,4 +17,5 @@ public class WsConstants {
     public static String EDIT_PROFILE = "EditProfile";
     public static String CHANGE_PASSWORD = "ChangePassword";
     public static String FORGOT_PASSWORD = "ForgotPassword";
+
 }
