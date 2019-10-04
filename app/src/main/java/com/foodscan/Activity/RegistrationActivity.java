698215f7a7aa75405428901e9774ccd5de157d9c
Pierre-Henry Soria <hi@ph7.me>
2019-10-04 21:52:59 +1300

    Update strings..

--- app/src/main/java/com/foodscan/Activity/RegistrationActivity.java
+++ app/src/main/java/com/foodscan/Activity/RegistrationActivity.java
@@ -71,7 +71,7 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
 
     private void initGlobals() {
 
-        String text = "<font color=#5B5B5B> By Creating Account, you are automatically accepting all the </font> <font color=#44B05B> Terms & Conditions</font>";
+        String text = "<font color=#5B5B5B> By Creating Account, you are automatically accepting all the </font> <font color=#44B05B> Terms of service</font>";
         txt_terms_and_condition.setText(Html.fromHtml(text));
 
         img_back.setOnClickListener(this);
